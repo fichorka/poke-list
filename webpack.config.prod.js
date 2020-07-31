e33073418a7133b20eab81eb9b3764a04195817c
fichorka <fichorka@gmail.com>
2020-07-31 03:42:51 +0200

    Optimize production css bundle

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -17,7 +18,8 @@ module.exports = {
       filename: './index.html',
       template: './src/html/index.html'
     }),
-    new MiniCssExtractPlugin({filename: 'bundle.css'})
+    new MiniCssExtractPlugin({filename: 'bundle.css'}),
+    new OptimizeCssAssetsPlugin()
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx', '.css']
